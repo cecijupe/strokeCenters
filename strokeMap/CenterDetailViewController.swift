81ab1ff7bda604e30eac4cd0649352408e9184ca
Jane Hall <drjanehall@gmail.com>
2015-12-10 23:11:19 -0800

    Edit DetailsView for Status Messages

--- strokeMap/CenterDetailViewController.swift
+++ strokeMap/CenterDetailViewController.swift
@@ -12,6 +12,7 @@ class CenterDetailViewController: UIViewController {
     
     // instance vars
     weak var goBackButtonDelegate: GoBackButtonDelegate?
+    var request = false
     
     // outlets
     @IBOutlet weak var nameLabel: UILabel!
@@ -27,10 +28,24 @@ class CenterDetailViewController: UIViewController {
     // did load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // fill in labels
         nameLabel.text = Hospital.name[Hospital.selected]
         timeLabel.text = "\(Hospital.timeTo[Hospital.selected]) min"
         distanceLabel.text = "\(Hospital.distance[Hospital.selected]) mi"
-        statusLabel.text = "\(Hospital.available[Hospital.selected])"
+        
+        // fill in status label
+        func writeStatusMessage()->String {
+            if Hospital.available[Hospital.selected] == false {            return "Not available"
+            }
+        else if Hospital.available[Hospital.selected] == true && request == false {
+                return "Available"
+            }
+        else if Hospital.available[Hospital.selected] == true && request == false {
+                return "Available"
+            }
+            
+        statusLabel.text = writeStatusMessage()
     }
     
     // memory warning
